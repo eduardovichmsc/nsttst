aabbea482bbec2b4a332e9f19c7955f24791de7d
Darzhan <eduardovdarzhan@gmail.com>
2025-02-21 20:50:39 +0500

    minor changes

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,5 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { ApiService } from './api.service';
+import { Request, Response } from 'express';
+import * as fs from 'fs';
+
+interface BodyType {
+  log: string;
+}
 
 @Controller('api')
 export class ApiController {
@@ -11,7 +17,26 @@ export class ApiController {
   }
 
   @Post()
-  async post(@Body() body: string) {
-    console.log(body);
+  async post(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Body() body: BodyType,
+  ) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+
+    const timestamp = new Date().toLocaleTimeString('ru-RU', { hour12: false });
+    const logEntry = `${timestamp} - ${body.log}\n`;
+
+    console.log(logEntry);
+    fs.appendFile('log.txt', logEntry, (err) => {
+      if (err) console.error('Ошибка записи в файл:', err);
+    });
   }
 }
+
+// ngrok http --host-header=rewrite 3000
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(7359);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
